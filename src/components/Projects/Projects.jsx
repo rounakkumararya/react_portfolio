76256acb0ddafff1ae5cbf14d07b6935790eb148
Rounak Kumar Arya <rounakkumar040@gmail.com>
2024-07-16 17:56:10 +0530

    edit projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -12,7 +12,7 @@ const Projects = () => {
   const { Projects } = content;
   return (
     <section className="bg-zinc-900 min-h-fit " id="projects">
-      <div className="  px-16  py-14 min-h-screen  flex flex-col justify-between">
+      <div className="  px-16  py-14  flex flex-col justify-between">
         <div>
           <h2
             className="title text-lime-500  lg:text-6xl text-start"
@@ -25,49 +25,49 @@ const Projects = () => {
           </h4>
           <br />
         </div>
-        <div className="flex items-center lg:flex-row flex-col-reverse gap-5">
-          <Swiper
-            pagination={{
-              clickable: true,
-            }}
-            modules={[Pagination]}
-            className="rounded-3xl pb-16 max-w-7xl  self-start"
-          >
-            {Projects.project_content.map((content, i) => (
-              <SwiperSlide
-                key={i}
-                className="bg-lime-500 rounded-3xl p-5 border-b-8 border-[#FAF9FD] h-5/6"
-              >
-                <img src={content.image} alt="..." />
-                <div className="flex flex-col gap-1 mt-2">
-                  <h5 className="font-bold font-Poppins">{content.title}</h5>
-                  <button className="font-bold text-gray self-end">
-                    READ MORE
-                  </button>
+
+        <Swiper
+          pagination={{
+            clickable: true,
+          }}
+          modules={[Pagination]}
+          className="rounded-3xl pb-16 max-w-7xl  self-start"
+        >
+          {Projects.project_content.map((content, i) => (
+            <SwiperSlide key={i} className="  ">
+              <article class="  bg-slate-200/70 mx-2 my-10 max-w-screen rounded-lg border border-white text-gray-700 shadow-md md:mx-auto">
+                <div class="flex flex-col md:flex-row">
+                  <div class="p-5 md:w-4/6 md:p-8">
+                    <span class="rounded-md bg-orange-400 px-2 py-1 text-xs uppercase text-white">
+                      {content.category}
+                    </span>
+                    <p class="mt-2 text-xl font-black md:mt-6 md:text-4xl">
+                      {content.title}
+                    </p>
+                    <p class="mt-3 text-gray-600">{content.description}</p>
+
+                    <button class="mt-4 mr-2 flex items-center justify-center rounded-md bg-lime-400 px-8 py-2 text-center text-white duration-150 md:mb-4 hover:translate-y-1 hover:bg-lime-500">
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        href={content.link}
+                      >
+                        View Project
+                      </a>
+                    </button>
+                  </div>
+                  <div class="mx-auto w-[70%] items-center px-5 md:flex md:p-8">
+                    <img
+                      class="rounded-md shadow-xl w-full"
+                      src={content.image}
+                      alt="Shop image"
+                    />
+                  </div>
                 </div>
-              </SwiperSlide>
-            ))}
-          </Swiper>
-        </div>
-      </div>
-      <div class="relative group">
-        <div class="overflow-hidden">
-          <div class="flex transition-transform duration-500 ease-in-out">
-            <img
-              src={content.image}
-              alt="Image 1"
-              class="w-full h-full object-cover"
-            />
-            <img
-              src="image2.jpg"
-              alt="Image 2"
-              class="w-full h-full object-cover"
-            />
-          </div>
-        </div>
-        <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-          <p class="text-white text-center">Image 1 Preview</p>
-        </div>
+              </article>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
